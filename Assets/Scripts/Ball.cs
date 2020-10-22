e184706303d2d6afc2cabdca7bd72dadc3140f40
Braxton Diggs <braxtondiggs@gmail.com>
2020-10-22 00:45:18 -0400

    Basic pong game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    Vector2 direction;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        direction = Vector2.one.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+       rb.velocity = direction * speed; 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Wall")) {
+            direction.y = -direction.y;
+        } else if (collision.gameObject.CompareTag("Paddle")) {
+            speed += Random.Range(1.5f, 1.5f);
+            direction.x = -direction.x;
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed;
+    public float minY;
+    public float maxY;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (gameObject.name == "rightPaddle") {
+            MovePaddle(gameObject.name);
+        } else if (gameObject.name == "leftPaddle") {
+            MovePaddle(gameObject.name);
+        }
+    }
+
+    void MovePaddle(string paddleName) {
+        float moveY = Input.GetAxisRaw(paddleName) * speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y + moveY, minY, maxY));
+    }
+} 
